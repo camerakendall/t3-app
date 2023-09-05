8a35887202967c8c81aa03e21a570b0ed4ab27af
Camera Kendall <camera@macbook-pro.mynetworksettings.com>
2023-09-05 19:58:46 -0400

    fixed bug in app file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,10 +6,10 @@ import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return 
+  return (
   <ClerkProvider {...pageProps}>
   <Component {...pageProps} />
   </ClerkProvider>
-};
+  )};
 
 export default api.withTRPC(MyApp);
